868eb3133e40e235f6af0fb47ecfe6c52df4b92e
Pedro Henrique Ribeiro <pedro.ribeiro33@fatec.sp.gov.br>
2024-09-23 18:28:50 -0300

    Criação da tabela etapas
    Criando o migrations e models de etapas

---
+++ backend/src/controllers/EtapaController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../database/data-source";
+import Etapas from "../models/Etapas";
+import Departamentos from "../models/Departamentos"; // Importar o model de Departamentos
+
+class EtapaController {
+
+    // Criar uma nova etapa
+    async create(req: Request, res: Response): Promise<Response> {
+        const { nome, id_departamento } = req.body;
+        const etapasRepository = AppDataSource.getRepository(Etapas);
+        const departamentosRepository = AppDataSource.getRepository(Departamentos);
+
+        try {
+            // Buscar o departamento pelo ID
+            const departamento = await departamentosRepository.findOneBy({ id: id_departamento });
+            
+            // Verificar se o departamento existe
+            if (!departamento) {
+                return res.status(404).json({ message: "Departamento não encontrado" });
+            }
+
+            // Criar nova etapa associada ao departamento
+            const etapa = etapasRepository.create({ nome, departamento });
+            await etapasRepository.save(etapa);
+
+            return res.status(201).json(etapa);
+
+        } catch (error) {
+            return res.status(500).json({ message: "Erro ao criar etapa", error });
+        }
+    }
+}
+
+export default new EtapaController();
--- backend/src/database/data-source.ts
+++ backend/src/database/data-source.ts
@@ -4,12 +4,15 @@ import User from "../models/User";
 import Role from "../models/Role";
 import Permission from "../models/Permission";
 import Departamentos from "../models/Departamentos";
+import Etapas from "../models/Etapas";
 import { CreateUsers1726672168699 } from "./migrations/1726672168699-CreateUsers";
 import { CreatePermissions1726679330555 } from "./migrations/1726679330555-CreatePermissions";
 import { CreateRoles1726679379911 } from "./migrations/1726679379911-CreateRoles";
 import { CreatePermissionsRoles1726679445904 } from "./migrations/1726679445904-CreatePermissionsRoles";
 import { CreateUsersRoles1726679463665 } from "./migrations/1726679463665-CreateUsersRoles";
 import { CreateDepartamentos1727086743759 } from "./migrations/1727086743759-CreateDepartamentos";
+import { CreateEtapas1727122773821 } from "./migrations/1727124734714-CreateEtapas";
+
 
 
 export const AppDataSource = new DataSource({
@@ -21,12 +24,13 @@ export const AppDataSource = new DataSource({
     database: "wecollab",
     synchronize: true,
     logging: false,
-    entities: [User, Role, Permission, Departamentos],
+    entities: [User, Role, Permission, Departamentos, Etapas],
     migrations: [CreateUsers1726672168699,
                 CreatePermissions1726679330555,
                 CreateRoles1726679379911,
                 CreatePermissionsRoles1726679445904,
                 CreateUsersRoles1726679463665,
-                CreateDepartamentos1727086743759],
+                CreateDepartamentos1727086743759,
+                CreateEtapas1727122773821],
     subscribers: [],
 })
