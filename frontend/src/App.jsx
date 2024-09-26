4e15d5d0b854f8dccc79e7be5e84dc5cae2ac5ef
Maria Luiza <mluizaguedessilva@gmail.com>
2024-09-26 16:04:42 -0300

    Add routes
    Add routing for main pages with BaseLayout integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import React from 'react';
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Portal from "./pages/Portal";
+import Settings from "./pages/Settings";
+import Timeline from "./pages/Timeline";
+import BaseLayout from './components/layout/BaseLayout';
+import SetorFinanceiro from './pages/SetorFinanceiro';
 
-import './App.css'
-import CadastrarDepartamentos from './components/cadastros/CadastrarDepartamento'
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
-      <CadastrarDepartamentos/>
-    </>
+    <BrowserRouter>
+        {/* rotas que não precisam do sidebar podem ser definidas aqui, ex: login */}
+        {/* <Route path='/login' exact element={<Login/>}></Route> */}
+
+      <Routes>
+        <Route element={<BaseLayout/>}>
+          <Route path='/' exact element={<Dashboard/>}></Route> {/* página inicial */}
+
+          <Route path='Monitoramento' exact element={<Timeline/>}></Route>
+          <Route path='Vendas' exact element={<Dashboard/>}></Route>
+          <Route path='SetorFinanceiro' exact element={<SetorFinanceiro/>}></Route>
+          <Route path='Portal' exact element={<Portal/>}></Route>
+
+          <Route path='Configurações' exact element={<Settings/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
