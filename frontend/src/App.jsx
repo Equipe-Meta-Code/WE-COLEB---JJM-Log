d50ebf09066c5ebf074ea309ed33b3c25e35383b
Maria Luiza <mluizaguedessilva@gmail.com>
2024-09-26 19:56:55 -0300

    Add "PageNotFound"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,23 +6,27 @@ import Settings from "./pages/Settings";
 import Timeline from "./pages/Timeline";
 import BaseLayout from './components/layout/BaseLayout';
 import SetorFinanceiro from './pages/SetorFinanceiro';
+import PageNotFound from './screens/PageNotFound';
 
 const App = () => {
   return (
     <BrowserRouter>
-        {/* rotas que não precisam do sidebar podem ser definidas aqui, ex: login */}
-        {/* <Route path='/login' exact element={<Login/>}></Route> */}
+        {/* rotas que não precisam do sidebar podem ser definidas aqui */}
+        
+        {/* <Route path='Login' exact element={<Login/>}></Route> */}
+        {/* <Route path="Cadastro" element={<Cadastro />} /> */}
 
       <Routes>
         <Route element={<BaseLayout/>}>
-          <Route path='/' exact element={<Dashboard/>}></Route> {/* página inicial */}
+          <Route path="*" element={<PageNotFound />} />
 
+          <Route path='/' exact element={<Timeline/>}></Route> {/* página inicial */}
           <Route path='Monitoramento' exact element={<Timeline/>}></Route>
           <Route path='Vendas' exact element={<Dashboard/>}></Route>
           <Route path='SetorFinanceiro' exact element={<SetorFinanceiro/>}></Route>
           <Route path='Portal' exact element={<Portal/>}></Route>
 
-          <Route path='Configurações' exact element={<Settings/>}></Route>
+          {/* <Route path='Configurações' exact element={<Settings/>}></Route> */}
         </Route>
       </Routes>
     </BrowserRouter>
