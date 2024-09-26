636df24fcdfd1f41dfe2c2bc3b43eb2d9de2afd2
Maria Luiza <mluizaguedessilva@gmail.com>
2024-09-26 16:58:55 -0300

    Update Sidebar component

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,203 +5,185 @@ import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { IoMenu } from "react-icons/io5";
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import { FaRoute, FaUser } from "react-icons/fa";
+import { FaRoute, FaUser, FaSignOutAlt } from "react-icons/fa";
+import { FaGear } from "react-icons/fa6";
 import { MdOutlineDashboard } from "react-icons/md";
 import { RiOrganizationChart } from "react-icons/ri";
-import { FaGear } from "react-icons/fa6";
-import { Link, useNavigate } from 'react-router-dom';
+import { Tooltip } from '@mui/material'; 
+import { Link } from 'react-router-dom';
+import './sideBar.css';  
 
-const drawerWidth = 240;
+const drawerWidth = 80;
 
-const openedMixin = (theme) => ({
+const Drawer = styled(MuiDrawer)(({ theme }) => ({
   width: drawerWidth,
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: (open && {'center' : "flex-start"}),
-  padding: theme.spacing(0, 1),
-  ...theme.mixins.toolbar,
-}));
-
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
+  flexShrink: 0,
+  whiteSpace: 'nowrap',
+  boxSizing: 'border-box',
+  backgroundColor: '#1B4215', 
+  color: 'white',
+  '& .MuiDrawer-paper': {
     width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
+    backgroundColor: '#1B4215',
+    color: 'white',
+    borderRadius: '35px',
+  },
+}));
 
 export default function Sidebar() {
   const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
 
   return (
     <nav>
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={()=>setOpen(!open)}>
-            {open ? <ChevronLeftIcon /> : <IoMenu />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {/* Monitoramento de pacote*/}
-          <ListItem disablePadding sx={{ display: "block"}}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <Link to="/Monitoramento" style={{ textDecoration: 'none', color: 'inherit' }}>
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <FaRoute size={18}/> {/* Icon */}
-              </ListItemIcon>
-                </Link>
-              <ListItemText primary="Monitoramento" sx={{ opacity: open ? 1 : 0}} />
-            </ListItemButton>
-          </ListItem>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <Drawer variant="permanent">
+          <Divider />
+          <List
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              height: '100%',
+            }}
+          >
+            <ListItem disablePadding sx={{ display: "block" }}>
+              <Tooltip title="Monitoramento de Pacote" placement="right" arrow>
+                <ListItemButton
+                  className="menu-link"
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Link to="/Monitoramento" className="menu-link-icon">
+                    <ListItemIcon
+                      sx={{
+                        justifyContent: "center",
+                        color: 'white',
+                      }}
+                    >
+                      <FaRoute size={19} />
+                    </ListItemIcon>
+                  </Link>
+                </ListItemButton>
+              </Tooltip>
+            </ListItem>
 
-          {/* Dashboard */}
-          <ListItem disablePadding sx={{ display: "block"}}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <Link to="/Vendas" style={{ textDecoration: 'none', color: 'inherit' }}>
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <MdOutlineDashboard size={18}/> {/* Icon */}
-              </ListItemIcon>
-              </Link>
-              <ListItemText primary="Vendas" sx={{ opacity: open ? 1 : 0}} />
-            </ListItemButton>
-          </ListItem>
+            <ListItem disablePadding sx={{ display: "block" }}>
+              <Tooltip title="Desempenho de Vendas" placement="right" arrow>
+                <ListItemButton
+                  className="menu-link"
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Link to="/Vendas" className="menu-link-icon">
+                    <ListItemIcon
+                      sx={{
+                        justifyContent: "center",
+                        color: 'white',
+                      }}
+                    >
+                      <MdOutlineDashboard size={19} />
+                    </ListItemIcon>
+                  </Link>
+                </ListItemButton>
+              </Tooltip>
+            </ListItem>
 
-          {/* Setor Financeiro */}
-          <ListItem disablePadding sx={{ display: "block"}}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <Link to="/SetorFinanceiro" style={{ textDecoration: 'none', color: 'inherit' }}>
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <RiOrganizationChart size={18}/> {/* Icon */}
-              </ListItemIcon>
-              </Link>
-              <ListItemText primary="Setor Financeiro" sx={{ opacity: open ? 1 : 0}} />
-            </ListItemButton>
-          </ListItem>
+            <ListItem disablePadding sx={{ display: "block" }}>
+              <Tooltip title="Setor Financeiro" placement="right" arrow>
+                <ListItemButton
+                  className="menu-link"
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Link to="/SetorFinanceiro" className="menu-link-icon">
+                    <ListItemIcon
+                      sx={{
+                        justifyContent: "center",
+                        color: 'white',
+                      }}
+                    >
+                      <RiOrganizationChart size={19} />
+                    </ListItemIcon>
+                  </Link>
+                </ListItemButton>
+              </Tooltip>
+            </ListItem>
 
-          {/* Portal do funcionário */}
-          <ListItem disablePadding sx={{ display: "block"}}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <Link to="/Portal" style={{ textDecoration: 'none', color: 'inherit' }}>
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <FaUser size={18}/> {/* Icon */}
-              </ListItemIcon>
-              </Link>
-              <ListItemText primary="Portal do funcionário" sx={{ opacity: open ? 1 : 0}} />
-            </ListItemButton>
-          </ListItem>
+            <ListItem disablePadding sx={{ display: "block" }}>
+              <Tooltip title="Portal do Funcionário" placement="right" arrow>
+                <ListItemButton
+                  className="menu-link"
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Link to="/Portal" className="menu-link-icon">
+                    <ListItemIcon
+                      sx={{
+                        justifyContent: "center",
+                        color: 'white',
+                      }}
+                    >
+                      <FaUser size={19} />
+                    </ListItemIcon>
+                  </Link>
+                </ListItemButton>
+              </Tooltip>
+            </ListItem>
 
-          {/* Configurações */}
-          {/* <ListItem disablePadding sx={{ display: "block"}}>
-            <ListItemButton
-              sx={{
-                minHeight: 48,
-                justifyContent: open ? "initial" : "center",
-                px: 2.5,
-              }}
-            >
-              <ListItemIcon
-                sx={{
-                  minWidth: 0,
-                  mr: open ? 3 : "auto",
-                  justifyContent: "center",
-                }}
-              >
-                <FaGear size={18}/>
-              </ListItemIcon>
-              <ListItemText primary="Configurações" sx={{ opacity: open ? 1 : 0}} />
-            </ListItemButton>
-          </ListItem> */}
-        </List>
-      </Drawer>
-    </Box>
+            <ListItem disablePadding sx={{ display: "block" }}>
+              <Tooltip title="Configurações" placement="right" arrow>
+                <ListItemButton
+                  className="menu-link"
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Link to="/Configurações" className="menu-link-icon">
+                    <ListItemIcon
+                      sx={{
+                        justifyContent: "center",
+                        color: 'white',
+                      }}
+                    >
+                      <FaGear size={19} />
+                    </ListItemIcon>
+                  </Link>
+                </ListItemButton>
+              </Tooltip>
+            </ListItem>
+
+            <ListItem disablePadding sx={{ display: "block" }}>
+              <Tooltip title="Sair" placement="right" arrow>
+                <ListItemButton
+                  className="menu-link"
+                  sx={{
+                    justifyContent: "center",
+                  }}
+                >
+                  <Link to="/Logout" className="menu-link-icon">
+                    <ListItemIcon
+                      sx={{
+                        justifyContent: "center",
+                        color: 'white',
+                      }}
+                    >
+                      <FaSignOutAlt size={19} />
+                    </ListItemIcon>
+                  </Link>
+                </ListItemButton>
+              </Tooltip>
+            </ListItem>
+          </List>
+        </Drawer>
+      </Box>
     </nav>
   );
 }
