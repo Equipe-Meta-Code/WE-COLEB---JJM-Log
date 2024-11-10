2e679eadaad83c72ea987f6466775d44302cf9cb
pedrohenribeiro <pedro.ribeiro33@fatec.sp.gov.br>
2024-11-10 14:30:02 -0300

    Ajuste de layout

--- frontend/src/components/cadastros/CadastroCliente.jsx
+++ frontend/src/components/cadastros/CadastroCliente.jsx
@@ -99,98 +99,137 @@ function CadastrarCliente() {
         <div className={styles.pagina}>
             <div className={styles.container}>
                 <h1 className={styles.tituloCard}>Cadastro de Clientes</h1>
+                
                 <div className={styles.campos}>
-                    <label>CNPJ:</label>
-                    <InputMask
-                        className={styles.inputTexto}
-                        mask="99.999.999/9999-99"
-                        placeholder="CNPJ"
-                        value={formData.cnpj}
-                        onChange={handleInputChange}
-                        name="cnpj"
-                    />
-                    <label>Razão Social:</label>
-                    <input
-                        className={styles.inputTexto}
-                        type="text"
-                        placeholder="Razão Social"
-                        value={formData.razao_social}
-                        onChange={handleInputChange}
-                        name="razao_social"
-                    />
-                    <label>Nome Fantasia:</label>
-                    <input
-                        className={styles.inputTexto}
-                        type="text"
-                        placeholder="Nome Fantasia"
-                        value={formData.nome_fantasia}
-                        onChange={handleInputChange}
-                        name="nome_fantasia"
-                    />
-                    <label>Inscrição Municipal:</label>
-                    <input
-                        className={styles.inputTexto}
-                        type="text"
-                        placeholder="Inscrição Municipal"
-                        value={formData.inscricao_municipal}
-                        onChange={handleInputChange}
-                        name="inscricao_municipal"
-                    />
-                    <label>Inscrição Estadual:</label>
-                    <input
-                        className={styles.inputTexto}
-                        type="text"
-                        placeholder="Inscrição Estadual"
-                        value={formData.inscricao_estadual}
-                        onChange={handleInputChange}
-                        name="inscricao_estadual"
-                    />
-                    <label>Contribuinte:</label>
-                    <select
-                        className={styles.selectTipoDocumento}
-                        value={formData.contribuinte}
-                        onChange={handleInputChange}
-                        name="contribuinte"
-                    >
-                        <option value="">Selecione</option>
-                        <option value="sim">SIM</option>
-                        <option value="nao">NÃO</option>
-                    </select>
-                    <label>Telefone:</label>
-                    <InputMask
-                        className={styles.inputTexto}
-                        type="text"
-                        mask="(99)99999-9999"
-                        placeholder="Telefone"
-                        value={formData.telefone}
-                        onChange={handleInputChange}
-                        name="telefone"
-                    />
-                    <label>E-mail:</label>
-                    <input
-                        className={styles.inputTexto}
-                        type="email"
-                        placeholder="Email"
-                        value={formData.email}
-                        onChange={handleInputChange}
-                        name="email"
-                    />
-                    <label>Natureza da operação:</label>
-                    <select
-                        className={styles.selectTipoDocumento}
-                        value={formData.natureza_operacao}
-                        onChange={handleInputChange}
-                        name="natureza_operacao"
-                    >
-                        <option value="">Selecione</option>
-                        <option value="Transportadora">Transportadora</option>
-                        <option value="Estabelecimento industrial">Estabelecimento industrial</option>
-                        <option value="Estabelecimento comercial">Estabelecimento comercial</option>
-                        <option value="Serviço de comunicação">Serviço de comunicação</option>
-                        <option value="Distribuidora de energia elétrica">Distribuidora de energia elétrica</option>
-                        <option value="Produtor rural">Produtor rural</option>
-                        <option value="Não contribuinte">Não contribuinte</option>
-                    </select>
+
+                <div className={styles.conjuntosCadastros}>
+                    <div className={styles.inputsCadastros}>
+                        <label>CNPJ:</label>
+                        <InputMask
+                            className={styles.inputTexto}
+                            mask="99.999.999/9999-99"
+                            placeholder="CNPJ"
+                            value={formData.cnpj}
+                            onChange={handleInputChange}
+                            name="cnpj"
+                        />
+                    </div>
+                    <div className={styles.inputsCadastros}>
+                        <label>Razão Social:</label>
+                        <input
+                            className={styles.inputTexto}
+                            type="text"
+                            placeholder="Razão Social"
+                            value={formData.razao_social}
+                            onChange={handleInputChange}
+                            name="razao_social"
+                        />
+
+                    </div>
+                    <div className={styles.inputsCadastros}>
+                        <label>Nome Fantasia:</label>
+                        <input
+                            className={styles.inputTexto}
+                            type="text"
+                            placeholder="Nome Fantasia"
+                            value={formData.nome_fantasia}
+                            onChange={handleInputChange}
+                            name="nome_fantasia"
+                        />
+
+                    </div>
+                    <div className={styles.inputsCadastros}>
+                        <label>Inscrição Municipal:</label>
+                        <input
+                            className={styles.inputTexto}
+                            type="text"
+                            placeholder="Inscrição Municipal"
+                            value={formData.inscricao_municipal}
+                            onChange={handleInputChange}
+                            name="inscricao_municipal"
+                        />
+
+                    </div>
+
+                    <div className={styles.inputsCadastros}>
+                        <label>Inscrição Estadual:</label>
+                        <input
+                            className={styles.inputTexto}
+                            type="text"
+                            placeholder="Inscrição Estadual"
+                            value={formData.inscricao_estadual}
+                            onChange={handleInputChange}
+                            name="inscricao_estadual"
+                        />
+
+                    </div>
+
+                    <div className={styles.inputsCadastros}>
+                        <label>Telefone:</label>
+                        <InputMask
+                            className={styles.inputTexto}
+                            type="text"
+                            mask="(99)99999-9999"
+                            placeholder="Telefone"
+                            value={formData.telefone}
+                            onChange={handleInputChange}
+                            name="telefone"
+                        />
+
+                    </div>
+
+                    <div className={styles.inputsCadastros}>
+                        <label>E-mail:</label>
+                        <input
+                            className={styles.inputTexto}
+                            type="email"
+                            placeholder="Email"
+                            value={formData.email}
+                            onChange={handleInputChange}
+                            name="email"
+                        />
+                        
+                    </div>
+                </div>
+                <div className={styles.conjuntosCadastros}>
+                    <div className={styles.inputsCadastros}>
+                        <label>Contribuinte:</label>
+                        <select
+                            className={styles.selectTipoDocumento}
+                            value={formData.contribuinte}
+                            onChange={handleInputChange}
+                            name="contribuinte"
+                        >
+                            <option value="">Selecione</option>
+                            <option value="sim">SIM</option>
+                            <option value="nao">NÃO</option>
+                        </select>
+
+                    </div>
+
+                    <div className={styles.inputsCadastros}>
+                        <label>Natureza da operação:</label>
+                        <select
+                            className={styles.selectTipoDocumento}
+                            value={formData.natureza_operacao}
+                            onChange={handleInputChange}
+                            name="natureza_operacao"
+                        >
+                            <option value="">Selecione</option>
+                            <option value="Transportadora">Transportadora</option>
+                            <option value="Estabelecimento industrial">Estabelecimento industrial</option>
+                            <option value="Estabelecimento comercial">Estabelecimento comercial</option>
+                            <option value="Serviço de comunicação">Serviço de comunicação</option>
+                            <option value="Distribuidora de energia elétrica">Distribuidora de energia elétrica</option>
+                            <option value="Produtor rural">Produtor rural</option>
+                            <option value="Não contribuinte">Não contribuinte</option>
+                        </select>
+
+                    </div>
+
+
+                </div>
+
                     <label>Ramo de Atividade:</label>
                     <input
                         className={styles.inputTexto}
